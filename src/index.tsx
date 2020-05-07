7c5fa595aa0a3858b0e9e9db9a8978b79f2229f2
M1core <44338439+M1core@users.noreply.github.com>
2020-05-07 15:22:05 +0300

    added local data and cheking logged in

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,27 @@
 import { ApolloClient } from 'apollo-client';
+import gql from 'graphql-tag';
 import { InMemoryCache, NormalizedCacheObject } from 'apollo-cache-inmemory';
 import { HttpLink } from 'apollo-link-http';
-import { ApolloProvider } from '@apollo/react-hooks';
+import { ApolloProvider, useQuery } from '@apollo/react-hooks';
 import React from 'react';
-import ReactDOM from 'react-dom'; 
+import ReactDOM from 'react-dom';
+import Login from './pages/login'; 
 import Pages from './pages';
 import injectStyles from './styles';
+import { resolvers, typeDefs } from './resolvers';
 
 const cache = new InMemoryCache();
 
+const IS_LOGGED_IN = gql`
+  query IsUserLoggedIn {
+    isLoggedIn @client
+  }
+`;
+
+function IsLoggedIn() {
+  const { data } = useQuery(IS_LOGGED_IN);
+  return data.isLoggedIn ? <Pages /> : <Login />;
+}
 
 const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
   cache,
@@ -18,6 +31,8 @@ const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
       authorization: localStorage.getItem('token'),
     }, 
   }),
+  typeDefs,
+  resolvers,
 });
 
 cache.writeData({
@@ -30,6 +45,7 @@ injectStyles();
 ReactDOM.render(
   <ApolloProvider client={client}>
     <Pages />
+    <IsLoggedIn />
   </ApolloProvider>, 
   document.getElementById('root')
 );
