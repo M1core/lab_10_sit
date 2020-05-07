42263a3a85d8d41e3ea4c6bcf9a4aa08ef392928
M1core <44338439+M1core@users.noreply.github.com>
2020-05-07 15:37:51 +0300

    added cart, cart button, logout button, prettyfied code, fixed login page

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,38 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 import { ApolloClient } from 'apollo-client';
-import gql from 'graphql-tag';
 import { InMemoryCache, NormalizedCacheObject } from 'apollo-cache-inmemory';
 import { HttpLink } from 'apollo-link-http';
 import { ApolloProvider, useQuery } from '@apollo/react-hooks';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Login from './pages/login'; 
+import gql from 'graphql-tag';
+
 import Pages from './pages';
-import injectStyles from './styles';
+import Login from './pages/login';
 import { resolvers, typeDefs } from './resolvers';
+import injectStyles from './styles';
 
 const cache = new InMemoryCache();
+const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
+  cache,
+  link: new HttpLink({
+    uri: 'http://localhost:4000/graphql',
+    headers: {
+      authorization: localStorage.getItem('token'),
+      'client-name': 'Space Explorer [web]',
+      'client-version': '1.0.0',
+    },
+  }),
+  resolvers,
+  typeDefs,
+});
+
+cache.writeData({
+  data: {
+    isLoggedIn: !!localStorage.getItem('token'),
+    cartItems: [],
+  },
+});
 
 const IS_LOGGED_IN = gql`
   query IsUserLoggedIn {
@@ -23,29 +45,11 @@ function IsLoggedIn() {
   return data.isLoggedIn ? <Pages /> : <Login />;
 }
 
-const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
-  cache,
-  link: new HttpLink({
-    uri: 'http://localhost:4000/graphql',
-    headers: {
-      authorization: localStorage.getItem('token'),
-    }, 
-  }),
-  typeDefs,
-  resolvers,
-});
-
-cache.writeData({
-  data: {
-    isLoggedIn: !!localStorage.getItem('token'),
-    cartItems: [],
-  },
-});
 injectStyles();
 ReactDOM.render(
+
   <ApolloProvider client={client}>
-    <Pages />
     <IsLoggedIn />
-  </ApolloProvider>, 
-  document.getElementById('root')
+  </ApolloProvider>,
+  document.getElementById('root'),
 );
